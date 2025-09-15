a78fbc3e5f766a796d7c12dbfb013a3317c88c60
Narendra Singh Nirwan <66245117+nsnirwan@users.noreply.github.com>
2025-09-15 17:18:08 +0530

    Update launch-ios-app.js

--- launch-ios-app.js
+++ launch-ios-app.js
@@ -1,16 +1,13 @@
 const wd = require("wd");
 
 const opts = {
-  path: "/wd/hub",
+  path: "/",
   port: 4723,
   capabilities: {
 
     platformName: "Mac",            // ✅ must be "iOS"
     automationName: "Mac2",     // ✅ correct driver
-    deviceName: "iPhone 14",        // match installed simulator
-    platformVersion: "13.4",        // check `xcrun simctl list` for actual version
-    app: "/Applications/AssessPrep.app", // must be a valid .app or .ipa
-    noReset: true
+    bundleId: "com.codeyug.assessprep-osx",
   }
 };
 
